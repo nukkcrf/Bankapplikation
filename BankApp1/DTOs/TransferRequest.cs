3e1f432fc4fe4abd57d13115ea6d72cabb483688
Gabriel Uifalean <gabriel13ro@gmail.com>
2025-04-22 13:42:21 +0200

    Beginning

--- BankApp1/DTOs/TransferRequest.cs
+++ BankApp1/DTOs/TransferRequest.cs
@@ -5,10 +5,21 @@
         public int FromAccountId { get; set; }
         public int ToAccountId { get; set; }
         public decimal Amount { get; set; }
+        public TransferRequest() { }
         public TransferRequest(int fromAccountId, int toAccountId, decimal amount)
         {
             FromAccountId = fromAccountId;
             ToAccountId = toAccountId;
+            Amount = amount;
+        }
+    }
+    public class AmountRequest
+    {
+        
+        public decimal Amount { get; set; }
+        public AmountRequest() { }
+        public AmountRequest(decimal amount)
+        {
             Amount = amount;
         }
     }
